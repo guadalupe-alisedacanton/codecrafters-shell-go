c4bef463aef8cd966cc2c24b99906dbdaaaf82eb
guadalupe-alisedacanton <guadalupe.aliseda@icloud.com>
2026-05-23 01:36:56 -0400

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,21 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
-  	"os"
+	"fmt"
+	"os"
 )
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	fmt.Print("$ ")
 
-
 	// Captures the user's command in the "command" variable
 	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 	if err != nil {
-		fmt.Println(command[:len(command)-1] + ": command not found")
+		fmt.Println(command + ": command not found")
 	}
-	
 
 }
