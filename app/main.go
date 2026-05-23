6df8d9e104b3bfd8edf4c0dea5feb3c520ac209a
guadalupe-alisedacanton <guadalupe.aliseda@icloud.com>
2026-05-23 01:44:54 -0400

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -7,15 +7,17 @@ import (
 )
 
 func main() {
-	// TODO: Uncomment the code below to pass the first stage
-	fmt.Print("$ ")
+	for {
+		fmt.Print("$ ")
 
-	// Captures the user's command in the "command" variable
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input:", err)
-		os.Exit(1)
+		// Captures the user's command in the "command" variable
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
-	fmt.Println(command[:len(command)-1] + ": command not found")
+	
 
 }
